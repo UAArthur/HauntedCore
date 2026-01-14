5a68fcdad7f8665d9831f9a4b42b70287b285bf5
UAArthur <shadowmerklingerarthur@gmail.com>
2026-01-14 21:47:04 +0100

    initial commit

---
+++ src/main/java/net/hntdstudio/dialogue/HytaleDialogue.java
@@ -0,0 +1,27 @@
+package net.hntdstudio.dialogue;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class HytaleDialogue extends JavaPlugin {
+
+    public HytaleDialogue(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+    @Override
+    protected void start() {
+        super.start();
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+    }
+}
